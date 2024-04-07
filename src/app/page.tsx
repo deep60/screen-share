17943c3287e4685bba58ef743c553f8c0abb1108
P Deepanshu <97436890+deep60@users.noreply.github.com>
2024-04-07 05:48:00 +0530

    adding the ability to query by tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { db } from "@/db";
 import { Room } from "@/db/schema";
 import { GithubIcon } from "lucide-react";
 import Link from "next/link";
+import { SearchBar } from "./search-bar";
 
 
 function RoomCard({ room }: { room: Room }) {
@@ -37,9 +38,9 @@ function RoomCard({ room }: { room: Room }) {
   )
 }
 
-export default async function Home() {
+export default async function Home({ searchParams, }: { searchParams : { search: string; }; }) {
 
-  const rooms = await getRooms();
+  const rooms = await getRooms(searchParams.search);
 
   return (
     <main className="min-h-screen p-16">
@@ -48,6 +49,9 @@ export default async function Home() {
         <Button asChild>
           <Link href="/create-room">Create Room</Link>
         </Button>
+      </div>
+      <div className="mb-12">
+        <SearchBar />
       </div>
       <div className="grid grid-col-3 gap-4">
       {rooms.map((room) => {
--- src/app/rooms/[roomId]/video-player.tsx
+++ src/app/rooms/[roomId]/video-player.tsx
@@ -5,6 +5,7 @@ import { Room } from "@/db/schema";
 import {
     Call,
     CallControls,
+    CallParticipantsList,
     SpeakerLayout,
     StreamCall,
     StreamTheme,
@@ -15,6 +16,7 @@ import {
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { generateTokenAction } from "./actions";
+import { useRouter } from "next/navigation";
 
 const apiKey = process.env.NEXT_PUBLIC_GET_STREAM_API_KEY!;
 
@@ -23,6 +25,7 @@ export function ScreenShareVideo({ room }: { room: Room }) {
     const session = useSession();
     const [client, setClient] = useState<StreamVideoClient | null>(null);
     const [call, setCall] = useState<Call | null>(null);
+    const router = useRouter();
 
     useEffect(() => {
         if (!room) return;
@@ -30,13 +33,23 @@ export function ScreenShareVideo({ room }: { room: Room }) {
             return;
         }
         const userId = session.data.user.id
-        const client = new StreamVideoClient({ apiKey, user: { id: userId }, tokenProvider: () => generateTokenAction()});
+        const client = new StreamVideoClient({ apiKey, 
+            user: { 
+                id: userId,
+                name: session.data.user.name ?? undefined,
+                image: session.data.user.image ?? undefined,
+            }, 
+            tokenProvider: () => generateTokenAction()});
         setClient(client);
         const call = client.call('default', room.id);
         call.join({ create: true });
+        setClient(client);
+        setCall(call);
         return () => {
-            call.leave();
-            client?.disconnectUser();
+            call
+                .leave()
+                .then(() => client.disconnectUser())
+                .catch(console.error);
         };
     }, [session, room])
 
@@ -46,7 +59,10 @@ export function ScreenShareVideo({ room }: { room: Room }) {
             <StreamTheme>
                 <StreamCall call={call}>
                     <SpeakerLayout />
-                    <CallControls />
+                    <CallControls onLeave={() => {
+                        router.push("/");
+                    }} />
+                    <CallParticipantsList onClose={() => undefined} />
                 </StreamCall>
             </StreamTheme>
         </StreamVideo>
